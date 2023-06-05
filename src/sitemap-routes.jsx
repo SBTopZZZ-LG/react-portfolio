e3106a7c5598e1c390598ee92d437ef71f5b4680
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-06-05 20:34:54 +0530

    chore: sitemap generation setup

---
+++ src/sitemap-routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+	<Route>
+		<Route path="/" />
+		<Route path="/about-me" />
+		<Route path="/works" />
+		<Route path="/credentials" />
+		<Route path="/contact" />
+	</Route>
+);
