6a5ce70b64c466b8cdaeeae6518acba0d2ec1b10
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-06-12 21:29:48 +0530

    refactor: add links to works tiles

--- src/Pages/WorksPage.jsx
+++ src/Pages/WorksPage.jsx
@@ -4,10 +4,14 @@ import Card from "../Components/Card";
 
 import data from "../Assets/Datasets/worksPage.json";
 
+import GitHub from "../Assets/Images/GitHub.svg";
+import Url from "../Assets/Images/Web.svg";
+
 const Tile = ({
 	dateRange = ["Jan 1970", "Jun 1970"],
 	title = "Project",
 	skills = ["Skill 1", "Skill 2"],
+	links = {},
 }) => {
 	return (
 		<div style={{
@@ -33,6 +37,45 @@ const Tile = ({
 				fontSize: "15px",
 				marginTop: "13px",
 			}}>Skills: {skills.join(", ")}</span>}
+
+			{("github" in links || "url" in links) &&
+				<div style={{
+					display: "flex",
+					alignItems: "center",
+					marginTop: "18px",
+					gap: "20px",
+				}}>
+					{"github" in links &&
+						<a
+							href={links.github}
+							style={{ all: "unset", cursor: "pointer" }}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="svg"
+								src={GitHub}
+								alt="GitHub"
+								height={35}
+							/>
+						</a>
+					}
+					{"url" in links &&
+						<a
+							href={links.url}
+							style={{ all: "unset", cursor: "pointer" }}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="svg"
+								src={Url}
+								alt="Url"
+								height={35}
+							/>
+						</a>
+					}
+				</div>}
 		</div>
 	);
 };
@@ -65,6 +108,7 @@ const Works = () => {
 							dateRange={data.date}
 							title={data.title}
 							skills={data.skills}
+							links={data.links}
 						/>
 					</Card>
 				))}
