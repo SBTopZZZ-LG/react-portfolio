348927cf2dca5b19dc2026e29dab3411942ca0c5
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-07-20 15:23:14 +0530

    feat: doc attachments

---
+++ src/Components/Attachment.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Attachment as AttachmentIcon } from "@material-ui/icons";
+
+import "./Attachment.css";
+
+/**
+ * @param {{ name: string, size: string }} document_type Document Type
+ * @returns Attachment
+ */
+const Attachment = ({
+	name,
+	file,
+	size,
+}) => {
+	return (
+		<div className="attachment-card">
+			{/* Icon */}
+			<div className="icon">
+				<AttachmentIcon />
+			</div>
+
+			{/* Content */}
+			<a
+				href={file}
+				target="_blank"
+				rel="noreferrer"
+				style={{ all: "unset", cursor: "pointer" }}
+			>
+				<div className="content">
+					<span className="file-name">{name}</span>
+					<span className="file-size">{`(${size})`}</span>
+				</div>
+			</a>
+		</div>
+	);
+};
+
+export default Attachment;
